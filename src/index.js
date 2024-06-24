b44a991116241c1d3b8f2cf6804260b3388e9e83
SergeySvetlov <sergey.svetlov.91@list.ru>
2024-06-24 23:16:21 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import {HashRouter} from 'react-router-dom';
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "../src/theme/default";
 
 import App from "./components/app/app"
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
+const rootElement = ReactDOM.createRoot(document.getElementById('root'));
+rootElement.render(
   <StrictMode>
-    <ThemeProvider theme={defaultTheme}>
-      <App />    
-    </ThemeProvider>
-  </StrictMode>,
-  rootElement
+    <HashRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <App />    
+      </ThemeProvider>      
+    </HashRouter>
+  </StrictMode>
 );
