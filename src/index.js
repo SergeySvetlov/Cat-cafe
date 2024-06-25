147ce8921a47296cb4a565688d5954bd3bef86d5
SergeySvetlov <sergey.svetlov.91@list.ru>
2024-06-25 14:47:11 +0300

    fix: init

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import { StrictMode } from "react";
-import ReactDOM from "react-dom/client";
-import {HashRouter} from 'react-router-dom';
+import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "../src/theme/default";
 
 import App from "./components/app/app"
 
-const rootElement = ReactDOM.createRoot(document.getElementById('root'));
-rootElement.render(
+const rootElement = document.getElementById("root");
+ReactDOM.render(
   <StrictMode>
-    <HashRouter>
-      <ThemeProvider theme={defaultTheme}>
-        <App />    
-      </ThemeProvider>      
-    </HashRouter>
-  </StrictMode>
+    <ThemeProvider theme={defaultTheme}>
+      <App />    
+    </ThemeProvider>
+  </StrictMode>,
+  rootElement
 );
